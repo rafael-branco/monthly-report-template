43daee6f841974ee782a16410931c81907a089c0
rafael-branco <rafael.rbranco@hotmail.com>
2024-08-05 17:58:54 -0300

    fixes

--- script.py
+++ script.py
@@ -1,3 +1,12 @@
 import pdfkit
 
-pdfkit.from_file('report.html', 'index.pdf', options={"enable-local-file-access": ""})
+options = {
+    'page-size': 'Letter',
+    "enable-local-file-access": "",
+    'margin-top': '0',
+    'margin-right': '0',
+    'margin-bottom': '0',
+    'margin-left': '0',
+}
+
+pdfkit.from_file('report.html', 'index.pdf', options=options)
